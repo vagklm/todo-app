d6fcd74a74a7fb488cb94e94845ce06d213314d1
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-06 17:52:36 +0300

    Changes!!

--- app/simple_project/models.py
+++ app/simple_project/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Simple_Project(models.Model):
     # Create your models here.
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
