5057773ebcc69ae7ed7ac0a571e23c419cc111be
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-07 15:39:44 +0300

    Changes made!

--- app/simple_project/urls.py
+++ app/simple_project/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from simple_project import views
 
 urlpatterns = [
-    path('', views.todo_list, name='todo_list'),
-    path('add/', views.add_task, name='add_task'),
-    path('complete/<int:todo_id>/', views.complete_task, name='complete_task'),
-    path('delete_completed_task/', views.delete_completed_tasks, name='delete_completed_tasks'),
+    path('todo_app/', views.todo_list, name='todo_list'),
+    path('todo_app/add/', views.add_task, name='add_task'),
+    path('todo_app/complete/<int:todo_id>/', views.complete_task, name='complete_task'),
+    path('todo_app/delete_completed_task/', views.delete_completed_tasks, name='delete_completed_tasks'),
 ]
