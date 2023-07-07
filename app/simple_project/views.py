6e9454d0e2f5d85c3982e3888d4972dda7795852
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-07 15:42:29 +0300

    Changes made!

--- app/simple_project/views.py
+++ app/simple_project/views.py
@@ -3,14 +3,14 @@ from .models import Simple_Project
 
 def todo_list(request):
     todos = Simple_Project.objects.all()
-    return render(request, 'simple_project/templates/todo.html', {'todos': todos})
+    return render(request, 'todo.html', {'todos': todos})
 
 def add_task(request):
     if request.method == "POST":
         title = request.POST['title']
         Simple_Project.objects.create(title=title)
         return redirect('todo')
-    return render(request, 'simple_project/templates/add_task.html')
+    return render(request, 'add_task.html')
 
 def complete_task(request, todo_id):
     todo = Simple_Project.objects.get(id=todo_id)
