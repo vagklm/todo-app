49cda8f711a343fc2d50fb3b5959c0e1345c44dc
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-07 15:55:48 +0300

    Updating url patterns

--- app/simple_project/urls.py
+++ app/simple_project/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('todo_app/', views.todo_list, name='todo_list'),
     path('todo_app/add/', views.add_task, name='add_task'),
     path('todo_app/complete/<int:todo_id>/', views.complete_task, name='complete_task'),
-    path('todo_app/delete_completed_task/', views.delete_completed_tasks, name='delete_completed_tasks'),
+    path('todo_app/completed_tasks/', views.completed_tasks, name='completed_tasks'),
 ]
