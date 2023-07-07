3bb926d090c1c7551329881b5a26100c85c78dd1
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-07 15:10:38 +0300

    Updating the url patterns for the app

--- app/simple_project/urls.py
+++ app/simple_project/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from simple_project import views
 
 urlpatterns = [
-    path('', views.simple_project, name='simple_project'),
+    path('', views.todo_list, name='todo'),
+    path('add/', views.add_task, name='add_task'),
+    path('complete/<int:todo_id>/', views.complete_task, name='complete_task'),
+    path('delete_completed_task/', views.delete_completed_tasks, name='delete_completed_tasks'),
 ]
