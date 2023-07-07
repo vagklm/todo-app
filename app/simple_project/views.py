8d2243a0ae3813d10a750b97caf5d360b12c7084
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-07 15:45:31 +0300

    Changes made!

--- app/simple_project/views.py
+++ app/simple_project/views.py
@@ -9,16 +9,16 @@ def add_task(request):
     if request.method == "POST":
         title = request.POST['title']
         Simple_Project.objects.create(title=title)
-        return redirect('todo')
+        return redirect('todo_list')
     return render(request, 'add_task.html')
 
 def complete_task(request, todo_id):
     todo = Simple_Project.objects.get(id=todo_id)
     todo.completed = True
     todo.save()
-    return redirect('todo')
+    return redirect('todo_list')
 
 def delete_completed_tasks(request):
     Simple_Project.objects.filter(completed=True).delete()
-    return redirect('todo')
+    return redirect('todo_list')
     
