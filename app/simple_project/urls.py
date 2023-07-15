05cfd633dbffd255029efce18d51a8a6e65233fc
Vag_kal96 <kalamatase@hotmail.gr>
2023-07-15 16:43:33 +0300

    changing url paths!

--- app/simple_project/urls.py
+++ app/simple_project/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from simple_project import views
 
 urlpatterns = [
-    path('todo_app/', views.todo_list, name='todo_list'),
-    path('todo_app/add/', views.add_task, name='add_task'),
-    path('todo_app/complete/<int:todo_id>/', views.complete_task, name='complete_task'),
-    path('todo_app/completed_tasks/', views.completed_tasks, name='completed_tasks'),
+    path('', views.todo_list, name='todo_list'),
+    path('add/', views.add_task, name='add_task'),
+    path('complete/<int:todo_id>/', views.complete_task, name='complete_task'),
+    path('completed_tasks/', views.completed_tasks, name='completed_tasks'),
 ]
